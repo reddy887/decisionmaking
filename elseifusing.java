6c05a771eb8eaec5d232e90179255b8786a8b8c2
reddy887 <avulareddybabu@gmail.com>
2025-07-19 22:11:58 +0530

    adding code eleseif

--- elseifusing.java
+++ elseifusing.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class elseifusing 
 {
     public static void main(String[] args) 
     {
-        
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the wether condition sunny or cloudy");
+        String check=sc.nextLine();
+        if(check.equals("sunny"))
+        {
+            System.out.println(check +"equal is sunny");
+        }
+        else if(check.equals("cloudy"))
+        {
+            System.err.println(check+" is equal to cloudy");
+        }
     }
 }
